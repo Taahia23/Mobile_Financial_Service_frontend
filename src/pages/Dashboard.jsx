b4f289d86823f746721f441551325546e7a72ab5
Taahia23 <taahiatahsin@gmail.com>
2025-02-26 20:27:40 +0600

    redirected into login page after logging out done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 const Dashboard = () => {
     const { user, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout(navigate)
+    }
   
     return (
       <div>
         <h1>Welcome, {user?.accountType}</h1>
-        <button onClick={logout}>Logout</button>
+        <button onClick={handleLogout}>Logout</button>
       </div>
     );
   };
