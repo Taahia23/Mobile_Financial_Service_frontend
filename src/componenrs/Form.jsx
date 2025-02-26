fd69fccf227681f20e6790806c4c5388b7cfd054
Taahia23 <taahiatahsin@gmail.com>
2025-02-26 21:28:50 +0600

    added functionality in trasaction form

--- src/componenrs/Form.jsx
+++ src/componenrs/Form.jsx
@@ -4,10 +4,25 @@ import axios from "axios";
 const TransactionForm = ({ type }) => {
   const [receiverMobile, setReceiverMobile] = useState("");
   const [amount, setAmount] = useState("");
- 
+  const token = localStorage.getItem("token");
+
+  const handleTransaction = async (e) => {
+    e.preventDefault();
+    try {
+      const url = `http://localhost:5000/api/transactions/${type}`;
+      await axios.post(url, { receiverMobile, amount }, { headers: { Authorization: `Bearer ${token}` } });
+
+      alert(`${type.replace("-", " ")} successful!`);
+      setReceiverMobile("");
+      setAmount("");
+    } catch (error) {
+      alert("Error: " + error.response.data.message);
+    }
+  };
 
   return (
     <form onSubmit={handleTransaction}>
+      <h3>{type.replace("-", " ")}</h3>
       <input type="text" placeholder="Receiver Mobile" value={receiverMobile} onChange={(e) => setReceiverMobile(e.target.value)} required />
       <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} required />
       <button type="submit">Submit</button>
